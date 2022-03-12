d186dbbeea70e85a732cf61f2974c9221e1e47de
shashank lalit patil <shashankpatil125@gmail.com>
2022-03-12 06:59:26 +0000

    nothing

---
+++ pages/components/Imagev.js
@@ -0,0 +1,9 @@
+
+
+function Imagev() {
+  return (
+    <image className="object-center m-auto p-10 pt-24 " src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt5ce6cb4e146af5db/61d898337c5435387a0c009d/Neon_1920x1080.jpg" alt="Girl in a jacket" width="500" height="600"/>
+  )
+}
+
+export default Imagev
