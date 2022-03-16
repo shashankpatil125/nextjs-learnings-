b3df7c92de12c11846183841e482dd3dfeba2904
shashank lalit patil <shashankpatil125@gmail.com>
2022-03-16 05:39:12 +0000

    wahi

--- pages/components/Details.js
+++ pages/components/Details.js
@@ -22,7 +22,7 @@ function Details() {
       <div id="myModal" className={usedisplay ? "" : "hidden" + " overflow-auto"}>
         <div className='flex mt-6'>
           <div className='w-56'>
-            <img className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/71wLsMcxA7L._SX679_.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+            <image className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/71wLsMcxA7L._SX679_.jpg" alt="Girl in a jacket" width="500" height="600"></image>
             <p className='text-gray-700'>VOYEE WE-07P Wireless PC Controller, Game Controller for PC PS3 PS2 Dualshock Joystick for Laptop PC Gaming, Wireless PC Gamepad (Red)</p>
             <div className=''>
               <button className="w-56 text-center my-2 bg-neutral-900 hover:bg-neutral-700 text-orange-600 font-bold  py-2 px-4 border-b-4 border-black hover:border-blue-500 rounded">Buy product</button>
@@ -33,7 +33,7 @@ function Details() {
           </div>
 
           <div className='w-56 ml-6'>
-            <img className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/71gyRfzBAKL._AC_SS450_.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+            <image className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/71gyRfzBAKL._AC_SS450_.jpg" alt="Girl in a jacket" width="500" height="600"></image>
             <p className='text-gray-700'>Cosmic Byte CB-GK-17 Galactic Wired Gaming Keyboard with Aluminium Body, 7 Color RGB Backlit with Effects, Anti-Ghosting (Black)</p>
             <div className=''>
               <button className="w-56 text-center my-2 bg-neutral-900 hover:bg-neutral-700 text-orange-600 font-bold  py-2 px-4 border-b-4 border-black hover:border-blue-500 rounded">Buy product</button>
@@ -44,7 +44,7 @@ function Details() {
           </div>
 
           <div className='w-56 ml-6'>
-            <img className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/81gGxv85cyL._SX679_.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+            <image className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/81gGxv85cyL._SX679_.jpg" alt="Girl in a jacket" width="500" height="600"></image>
             <p className='text-gray-700'>
               Roll over image to zoom in
               ASUS TUF Gaming GeForce GTX 1650 4GB GDDR6 RAM Non LHR Graphic Card 128 Bit 896 CUDA Cores</p>
@@ -57,7 +57,7 @@ function Details() {
           </div>
 
           <div className='w-56 ml-6'>
-            <img className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/71h7F81EBoS._AC_UY327_FMwebp_QL65_.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+            <image className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/71h7F81EBoS._AC_UY327_FMwebp_QL65_.jpg" alt="Girl in a jacket" width="500" height="600"></image>
             <p className='text-gray-700'>Dell G15 Intel i5-10500H Gaming Laptop, 16GB DDR4, 512GB SSD, Win 10, NVIDIA GTX 1650 4GB GDDR6, 15.6 (39.62cm)</p>
             <div className=''>
               <button className="w-56 text-center my-2 bg-neutral-900 hover:bg-neutral-700 text-orange-600 font-bold  py-2 px-4 border-b-4 border-black hover:border-blue-500 rounded">Buy product</button>
@@ -68,7 +68,7 @@ function Details() {
           </div>
 
           <div className='w-56 ml-6'>
-            <img className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/51zb2RLO-hS.jpg" alt="Girl in a jacket" width="500" height="600"></img>
+            <image className="  p-4  h-56 w-56 bg-white rounded-xl" src="https://m.media-amazon.com/images/I/51zb2RLO-hS.jpg" alt="Girl in a jacket" width="500" height="600"></image>
             <p className='text-gray-700'>Offbeat®- Ripjaw Alpha Gaming Series 2.4gh Wireless Rechargeable, 8D Silent Clicks, DPI 1600,2400,3200 Mouse for Pc/Laptop/Smart TV/Mac</p>
             <div className=''>
               <button className="w-56 text-center my-2 bg-neutral-900 hover:bg-neutral-700 text-orange-600 font-bold  py-2 px-4 border-b-4 border-black hover:border-blue-500 rounded">Buy product</button>
