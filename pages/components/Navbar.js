7d3626158cc23e447abddd9d387d5ebe450dc46f
shashank lalit patil <shashankpatil125@gmail.com>
2022-03-18 14:38:54 +0000

    state

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -14,16 +14,38 @@ function Navbar() {
   }
   const router = useRouter();
 
-  function drop(){
-    var c=0;
-    
-    var x=document.getElementById('dropdown')
-    var nv=document.getElementById('nav');{
-      if(x.style.visibility=='visible'){
-        x.style.visibility='hidden';
+  let pro='hidden'
+  const [dis, setdisplay] = useState('hidden')
+  function t() {
+    console.log("asfd");
+
+    {
+      if (dis == 'hidden') {
+        setdisplay('visible')
+        return
+      }
+      else if (dis == false) {
+        setdisplay('hidden')
+        return
       }
       else {
-        x.style.visibility='visible';
+        setdisplay('hidden')
+      }
+    }
+  }
+
+
+
+  function drop() {
+    var c = 0;
+
+    var x = document.getElementById('dropdown')
+    var nv = document.getElementById('nav'); {
+      if (x.style.visibility == 'visible') {
+        x.style.visibility = 'hidden';
+      }
+      else {
+        x.style.visibility = 'visible';
       }
     }
   }
@@ -52,25 +74,28 @@ function Navbar() {
 
 
         <div className='lg:hidden  md:hidden'>
-          <button id="dropdownButton" data-dropdown-toggle="dropdown"  className="mt-3 ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 lg:hidden md-hidden sm-hidden" type="button" onClick={drop} >
-          <i className="iconify" data-icon="akar-icons:three-line-horizontal"></i>
+          <button id="dropdownButton" data-dropdown-toggle="dropdown" className="mt-3 ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 lg:hidden md-hidden sm-hidden" type="button" onClick={t} >
+            <i className="iconify" data-icon="akar-icons:three-line-horizontal"></i>
           </button>
 
-          <div className=" ml-4 z-10  text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 " id="dropdown">
-            <ul className="py-1" aria-labelledby="dropdownButton">
-              <li>
-                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Explore</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Manifesto</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Developers</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Zine</a>
-              </li>
-            </ul>
+
+          <div className={dis}>
+            <div className=" ml-4 z-10  text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 " id="dropdown">
+              <ul className="py-1" aria-labelledby="dropdownButton">
+                <li>
+                  <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Explore</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Manifesto</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Developers</a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Zine</a>
+                </li>
+              </ul>
+            </div>
           </div>
 
         </div>
